d1f3fff3af45418cc0dcd68a828e65f541356705
Randell Quitain <cprjk.buzz@gmail.com>
2020-03-23 03:10:57 +0800

    More performance enhancement

--- pages/index.vue
+++ pages/index.vue
@@ -57,13 +57,7 @@ export default {
     onToggle() {
       this.isOpen = !this.isOpen
     }
-  },
-  mounted() {
-    this.$nextTick(() => {
-      this.$nuxt.$loading.start()
-      setTimeout(() => this.$nuxt.$loading.finish(), 500)
-    })
-  },
+  }
 }
 </script>
 
